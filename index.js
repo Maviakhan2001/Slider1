f2619f79c62ea4483ebe5a1d6f213ca447ba944b
Mavia Khan <113515670+Maviakhan2001@users.noreply.github.com>
2022-11-11 11:43:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+var indexValue = 1;
+showImage(indexValue);
+
+function btnSlide(e){
+    showImage(indexValue = e);
+}
+
+function showImage(e) {
+    var i;
+    const img = document.querySelectorAll('img');
+    const slider = document.querySelectorAll('.btn-slider span');
+    
+    if(e > img.length){
+        indexValue = 1;
+    }
+    
+    if(e < 1){
+        indexValue = img.length;
+    }
+    
+    for(i = 0; i < img.length; i++){
+        img[i].style.border = "2px transparent"
+    }
+    
+    // content.scrollBy(1000, 0)
+    img[indexValue-1].style.border = "2px solid red";
+    img[indexValue-1].style.margin = "0rem 10rem"
+
+}
